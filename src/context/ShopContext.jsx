c88c076e9976000e22e0ec34ea134a5827e01994
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-08-24 10:11:35 +0500

    Add dynamic cart display, quantity update, item removal, and checkout functionality with responsive styling.

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../frontend_assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 // Create the context
 export const ShopContext = createContext();
@@ -11,6 +12,7 @@ const ShopContextProvider = ({ children }) => {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(true);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId, productSize) => {
         if (!productSize) {
@@ -30,6 +32,7 @@ const ShopContextProvider = ({ children }) => {
             cartData[itemId][productSize] = 1;
         }
 
+        // console.log(cartData);
         setCartItems(cartData);
     };
 
@@ -37,17 +40,42 @@ const ShopContextProvider = ({ children }) => {
         let totalCount = 0;
         for (const items in cartItems) {
             for (const item in cartItems[items]) {
+                // console.log('cartItems[items]', cartItems[items]);
                 try {
                     if (cartItems[items][item] > 0) {
                         totalCount += cartItems[items][item];
+                        // console.log('cartItems[items][item]', cartItems[items][item]);
                     }
                 } catch (error) {
                     console.error("Error counting cart items:", error);
                 }
             }
         }
+
         return totalCount;
     };
+
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData);
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items]) {
+                if (cartItems[items][item] > 0) {
+                    totalAmount += itemInfo.price * cartItems[items][item];
+                }
+            }
+        }
+
+        // console.log(totalAmount);
+        return totalAmount;
+    }
 
     const value = {
         products,
@@ -60,6 +88,9 @@ const ShopContextProvider = ({ children }) => {
         cartItems,
         addToCart,
         getCartCount,
+        updateQuantity,
+        getCartAmount,
+        navigate,
     };
 
     return (
