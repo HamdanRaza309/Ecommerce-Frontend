c3a4defe1aeca84c3699c858f2012d7a7903bc0e
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-08-25 07:56:33 +0500

    Add Orders with product listing and order tracking functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-// import { assets } from '../frontend_assets/assets'
+import { assets } from '../frontend_assets/assets'
 
 function Footer() {
     return (
         <>
             <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
                 <div>
-                    {/* <img src={assets.logo} alt="logo" className="mb-5 w-32" /> */}
-                    <h1>Logo</h1>
+                    <img className='mb-5 w-52' src={assets.logo} alt="logo" />
                     <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto et vero rerum, est reprehenderit in voluptas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium similique minima dicta minus quasi, commodi suscipit dolores ullam nesciunt repudiandae?</p>
                 </div>
                 <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,9 @@ function Navbar() {
 
     return (
         <div className="flex items-center justify-between py-5 font-medium">
-            <Link to='/'>Title</Link>
+            <Link to='/'>
+                <img className='w-48' src={assets.logo} alt="logo" />
+            </Link>
             <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
                 <NavLink to='/' className='flex flex-col gap-1 items-center'>
                     <p>HOME</p>
