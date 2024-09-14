2e72a801951c8782391668b9858ec25e383c3cfb
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-09-14 10:51:37 +0500

    Add BusinessFocus component to display main focuses and refactor Achievement component for a new look.

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
 // const URI = 'mongodb://localhost:27017/fashion-closet'; //compassURI
-const URI = 'mongodb+srv://fashion-closet:fashion-closet@cluster0.2kmzsps.mongodb.net/fashion-closet' //atlasURI
-
+// const URI = 'mongodb+srv://fashion-closet:fashion-closet@cluster0.2kmzsps.mongodb.net/fashion-closet' //atlasURI
+const URI = `mongodb+srv://hamdanraza309:AaOUY8RWUNV9FpQo@fashion-closet.na4w4.mongodb.net/fashion-closet`
 const connectToMongoDb = async () => {
     try {
         const mongoURI = process.env.MONGO_URI || URI;
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(mongoURI);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Failed to connect to MongoDB:', error.message);
